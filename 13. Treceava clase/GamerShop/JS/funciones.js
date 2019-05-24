9f77cf13bc908e5bc710ef4e98faba1e68343cc5
gguevarav <gemisdguevarav@gmail.com>
2019-05-24 13:24:37 -0600

    Se agregan las imágenes y funcionamiento de las páginas principales, pendiente que llene el carrito de compra.

--- 13. Treceava clase/GamerShop/JS/funciones.js
+++ 13. Treceava clase/GamerShop/JS/funciones.js
@@ -110,7 +110,6 @@ function MostrarArticulos(Grupo) {
 }
 
 function MostrarModal(Articulo, Titulo, CasaComercial, Precio) {
-	//var Contenido = "	<div class='row'>	<div class='col-lg-5'>	<div class='modal-body'>	<img src='IMG/" + articulo + "/" + articulo + "2.jpg' class='img-fluid'>	</div>	</div>	<div class='col-lg-7'>	<div class='titulo'>	<form>	<div class='card'>	<div class='card-body'>	<h5 class='card-title'>" + Titulo + "</h5>	<hr>	<small class='card-text'>by" + CasaComercial + "</small>	<span>	<ion-icon name='star'></ion-icon>	<ion-icon name='star'></ion-icon>	<ion-icon name='star'></ion-icon>	<ion-icon name='star'></ion-icon>	<ion-icon name='star'></ion-icon>	</span>	<h4 class='card-text PrecioArticulo' id='PrecioArticulo'>Q. " + Precio + "</h4>	<p class='card-text'>Cantidad</p>	<input type='number' class='form-control' id='Cantidad' name='Cantidad' placeholder='Cantidad' required>	<hr>	<a href='#' class='btn btn-primary'>Agregar al carrito</a>	</div>	</div>	</form>	</div>	</div>	</div>"
 	// Cambiamos valores a objetos
 	// Cambiamos la imagen
 	$(".ImagenVistaPrevia").attr("src","IMG/" + Articulo + "/" + Articulo +"2.jpg");
@@ -122,4 +121,73 @@ function MostrarModal(Articulo, Titulo, CasaComercial, Precio) {
 	$('.PrecioArt').text('Q. ' + Precio);
 	//document.querySelector("#PrecioArticulo").innerText = 'asd';
 	$("#AgregarCarrito").modal("show");
+}
+
+$("#Agregar").click(function() {
+
+	$("#ArticuloComprar").clone().appendTo("#CarritoCompra");
+});
+
+ContadorNumeroFila = 0;
+function crear(obj) {
+    // Aumentamos el 1 el contador de la fila
+    ContadorNumeroFila++;
+    // Obtenemos el nombre del id donde queremos agregarlo
+    CuerpoTabla = document.getElementById('CuerpoTabla');
+    // Creamos la fila para agregar al cuerpo de la tabla
+    FilaTabla = document.createElement('tr');
+    //Agregamos el número de la fila a la fila
+    NumeroFila = document.createElement('th');
+    NumeroFila.scope = 'row';
+    NumeroFila.innerHTML = ContadorNumeroFila;
+    // Creamos la primer columna de la fila
+    ColumnaFila1 = document.createElement('td');
+    DivInputGroup = document.createElement('div');
+    DivInputGroup.className = 'input-group input-group-lg';
+    SpanSizing = document.createElement('span');
+    SpanSizing.id = 'sizing-addon1';
+    SpanSizing.className = 'input-group-addon';
+    Icono = document.createElement('i');
+    Icono.className = 'glyphicon glyphicon-asterisk';
+    NombreArticulo = document.createElement('p');
+    //$('#TituloArticulo h5').clone().append(NombreArticulo);
+    // SelectProducto = document.createElement('select');
+    NombreArticulo.id = 'TituloArticulo' + ContadorNumeroFila;
+    NombreArticulo.className = 'card-title TituloArticulo';
+    NombreArticulo.name = 'TituloArticulo' + ContadorNumeroFila;
+    NombreArticulo.innerHTML = document.getElementById('TituloArticulo').val();
+    
+    //$('#Equipo1 option').clone().appendTo(SelectProducto);
+    // Para el select
+    //SpanSizing.appendChild(Icono);
+    //DivInputGroup.appendChild(SpanSizing);
+    DivInputGroup.appendChild(NombreArticulo);
+    ColumnaFila1.appendChild(DivInputGroup);
+    
+    // Creamos la tercer columna
+    ColumnaFila3 = document.createElement('td');
+    DivInputGroup3 = document.createElement('div');
+    DivInputGroup3.className = 'input-group input-group-lg';
+    SpanSizing3 = document.createElement('span');
+    SpanSizing3.id = 'sizing-addon1';
+    SpanSizing3.className = 'input-group-addon';
+    Icono3 = document.createElement('i');
+    Icono3.className = 'glyphicon glyphicon-question-sign';
+    InputHoras = document.createElement('input');
+    InputHoras.id = 'CantidadHorasEquipo' + ContadorNumeroFila;
+    InputHoras.className = 'form-control';
+    InputHoras.name = 'CantidadHorasEquipo' + ContadorNumeroFila;
+    InputHoras.placeholder = 'Horas';
+    InputHoras.type = 'number';
+    SpanSizing3.appendChild(Icono3);
+    DivInputGroup3.appendChild(SpanSizing3);
+    DivInputGroup3.appendChild(InputHoras);
+    ColumnaFila3.appendChild(DivInputGroup3);
+    FilaTabla.appendChild(NumeroFila);
+    // Agregamos las dos columnas a la fila
+    FilaTabla.appendChild(ColumnaFila1);
+
+    FilaTabla.appendChild(ColumnaFila3);
+    // Agregamos la fila al cuerpo de la tabla
+    CuerpoTabla.appendChild(FilaTabla);
 }
